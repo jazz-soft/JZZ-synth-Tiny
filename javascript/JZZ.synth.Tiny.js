d17346a40c6545da716287ab6ebade6b2f4c8ee3
Sema <sema@pyva.net>
2018-11-03 00:40:46 -0400

    Updated dependencies

--- javascript/JZZ.synth.Tiny.js
+++ javascript/JZZ.synth.Tiny.js
@@ -14,7 +14,7 @@
   if (!JZZ.synth) JZZ.synth = {};
   if (JZZ.synth.Tiny) return;
 
-  var _version = '1.0.2';
+  var _version = '1.0.3';
 
 function WebAudioTinySynth(opt){
   this.__proto__ = this.sy =
