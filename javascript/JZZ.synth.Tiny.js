2ece0217bcae513f1fb4abdb0952d5583076661f
Sema <jazz-dev@jazz-soft.net>
2020-08-05 20:27:11 -0400

    New release

--- javascript/JZZ.synth.Tiny.js
+++ javascript/JZZ.synth.Tiny.js
@@ -14,7 +14,7 @@
   if (!JZZ.synth) JZZ.synth = {};
   if (JZZ.synth.Tiny) return;
 
-  var _version = '1.1.8';
+  var _version = '1.1.9';
 
 function WebAudioTinySynth(opt){
   this.__proto__ = this.sy =
